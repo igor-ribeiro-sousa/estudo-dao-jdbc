5666e1495e146d1e262fb8f0e4d7a41fd428d1f3
Igor Ribeiro <igorribeirocons@gmail.com>
2023-05-26 23:12:27 -0300

    implementação Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import modal.entities.Department;
+import modal.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(1, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+
+		
+		System.out.println(seller);
 	}
 
 }
