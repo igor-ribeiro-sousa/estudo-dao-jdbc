587c695d96271b1d3abe1d956343f67e59da37f4
Igor Ribeiro <igorribeirocons@gmail.com>
2023-05-27 11:49:02 -0300

    Melhorias de implementacao do findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,7 @@ public class Program {
 
 		SellerDao selerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = selerDao.findById(3);
 		System.out.println(seller);
 		
