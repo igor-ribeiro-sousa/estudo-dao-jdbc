2e21aa1bd8c0488ebb07d257d50c8332dd31c1ff
Igor Ribeiro <igorribeirocons@gmail.com>
2023-05-31 00:10:50 -0300

    Implementação Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import modal.entities.Department;
 import modal.entities.Seller;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 
 public class Program {
 
@@ -28,6 +30,12 @@ public class Program {
 		System.out.println("\n=== TEST 3: seller findAll ===");
 		list = selerDao.findAll();
 		list.forEach(System.out::println);
+		
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Mirna", "mirna@gmail.com", new Date(), 4000.0, dep);
+		selerDao.insert(newSeller);
+		System.out.println("Sucesso! Novo id = " + newSeller.getId());
 		
 	}
 
